4a946ab81c68a7f163164fe503440df37efc6cc0
malloryjustis <majustis@davidson.edu>
2022-04-03 22:52:11 -0400

    Create fetch_weather.py

---
+++ fetch_weather.py
@@ -0,0 +1,8 @@
+from requests import get
+import json
+from pprint import pprint
+
+url = 'https://apex.oracle.com/pls/apex/raspberrypi/weatherstation/getlatestmeasurements/1100597'
+
+weather = get(url).json()['items']
+pprint(weather)
