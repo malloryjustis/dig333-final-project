5a3cbcc713bc2f6312d64442e5b40eb208f5f6aa
malloryjustis <majustis@davidson.edu>
2022-04-06 01:37:12 -0400

    Create weathertoprogrammablecode.py

---
+++ weathertoprogrammablecode.py
@@ -0,0 +1,13 @@
+from requests import get
+import json
+from pprint import pprint
+
+url = 'https://api.weather.gov/gridpoints/GSP/116,76/forecast'
+
+weather = get(url).json()['properties']
+#pprint(weather)
+
+print(weather.values(0))
+
+
+
