2a7c3de0704a7e69178ec840df919a3e5fb940af
malloryjustis <98776627+malloryjustis@users.noreply.github.com>
2022-04-26 23:06:45 -0400

    Update weather_to_gpio.py

--- python_codes/weather_to_gpio.py
+++ python_codes/weather_to_gpio.py
@@ -10,33 +10,33 @@ GPIO.setmode(GPIO.BOARD)
 current_weather = c.current_weather_data_only()
 
 #porch lights turning on/off
-GPIO.setup(7, GPIO.OUT)
+GPIO.setup(1, GPIO.OUT)
 
 if current_weather['isDaytime'] == True:
     #porch lights off
-    GPIO.output(7,GPIO.LOW)
+    GPIO.output(1,GPIO.LOW)
     print('porch OFF')
 else:
-    GPIO.output(7,GPIO.HIGH)
+    GPIO.output(1,GPIO.HIGH)
     print('porch ON')
     
 #fan turning on/off
-GPIO.setup(13, GPIO.OUT)
+GPIO.setup(2, GPIO.OUT)
 
 while current_weather['temperature'] > 69:
     #turn DC fan on
-    GPIO.output(13,GPIO.HIGH)
+    GPIO.output(2,GPIO.HIGH)
     print('fan ON')
 
 #fireplace turning on/off
-GPIO.setup(11, GPIO.OUT)
+GPIO.setup(17, GPIO.OUT)
 
 if current_weather['temperature'] < 46:
     #turn fireplace on
-    GPIO.output(11,GPIO.HIGH)
+    GPIO.output(17,GPIO.HIGH)
     print('fireplace ON')
 else:
-    GPIO.output(11,GPIO.LOW)
+    GPIO.output(17,GPIO.LOW)
     print('fireplace OFF')
 
 #blinds
