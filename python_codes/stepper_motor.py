02563d20c7ccff8bd101c6a85e2e0b95665e88f8
malloryjustis <98776627+malloryjustis@users.noreply.github.com>
2022-05-06 14:15:49 -0400

    Create stepper_motor.py

---
+++ python_codes/stepper_motor.py
@@ -0,0 +1,31 @@
+from time import sleep
+import RPi.GPIO as GPIO
+
+DIR = 20 # direction gpio pin
+STEP = 21 # step gpio pin
+CW = 1 # clockwise rotation
+CCW = 0 # counterclockwise rotation
+SPR = 48 # steps per revolution (360 / 7.5)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(DIR, GPIO.OUT)
+GPIO.setup(STEP, GPIO.OUT)
+GPIO.output(DIR, CW)
+
+step_count = SPR
+delay = 0.0208
+
+for x in range(step_count):
+  GPIO.OUTPUT(STEP, GPIO.HIGH)
+  sleep(delay)
+  GPIO.OUTPUT(STEP, GPIO.LOW)
+  sleep(delay)
+  
+sleep(0.5)
+GPIO.output(DIR, CCW)
+
+for x in range(step_count):
+  GPIO.OUTPUT(STEP, GPIO.HIGH)
+  sleep(delay)
+  GPIO.OUTPUT(STEP, GPIO.LOW)
+  sleep(delay)
